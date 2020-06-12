01be11cc8512c46f7853c3b0274b45a42a93ac04
sam wang <muumlover@live.com>
2020-06-12 16:02:45 +0800

    fix

--- tencent-video/test.py
+++ tencent-video/test.py
@@ -31,7 +31,7 @@
 """
 
 from easywasm import pywasm_path
-from tx_player import TxPlayer
+from tx_player import TxPlayer, Wasm
 
 pywasm_path()
 
@@ -39,8 +39,9 @@ if __name__ == '__main__':
     tx_player = TxPlayer('https://v.qq.com/x/cover/bzfkv5se8qaqel2/n0020yrnly7.html')
     print(tx_player._c_key_8_1)
     # print(tx_player._c_key_9_1)
-    key = tx_player.wasm.wa__getkey(
-        tx_player.platform, tx_player.appVer, tx_player.vid, '', '1fcb9528b79f2065c9a281a7d554edd1', 1556590422)
+    key = Wasm(tx_player).wa__getkey(
+        tx_player.platform, tx_player.appVer, tx_player.vid, '', '1fcb9528b79f2065c9a281a7d554edd1', 1556590422
+    )
     print(key)
     key2 = '88dkKwGYKDF79ZEItZs_lpJX5WB4a2CdS8k5hHANVaqtHEZQ1c_W6myJ8hQDnmDDHYlsFcmCbTs52vPBr-xE-uhvZyEMY131vUh1H4pgCXe2OpAF-Wzte7F4kwln76xq_nwqEERWEZPLluNDEH6IC8EOljLQ2VfW2sTdospNPlD9535CNT9iSo3cLRH93ogtX_ObeZBTEOuKEsbtjkFpGl3F3IxmISJc_8dRIBruTik-e4rt0isxZAXexKqWDJGxu2rhTvrXyHJH87s1dUoA6Stf9X0c1RYGSdj_UQOME-ds0E7OepYmW0kNjO5DljtDJlQGBgYGBgZEwDLM'
     print(key == key2)
