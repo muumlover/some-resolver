148404f802b25f7c3e5426c97fd26380123387ce
sam wang <muumlover@live.com>
2020-06-12 16:03:29 +0800

    fix

--- tencent-video/test_ckey.py
+++ tencent-video/test_ckey.py
@@ -7,7 +7,7 @@
 @Email   : muumlover@live.com
 @Blog    : https://blog.ronpy.com
 @Project : xbmc-addons
-@FileName: test.py
+@FileName: test_ckey.py
 @Software: PyCharm 
 @license : (C) Copyright 2020 by Sam Wang. All rights reserved.
 @Desc    : 
